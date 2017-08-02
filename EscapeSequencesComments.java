995abfec68183e462b6c91add3d22764e70be962
Alexis Low <alexisthelow@gmail.com>
2017-08-02 14:53:37 -0600

    oops i fix it

--- EscapeSequencesComments.java
+++ EscapeSequencesComments.java
@@ -8,7 +8,7 @@ public class EscapeSequencesComments {
 		System.out.print("Jello\by\n"); // This line prints Jelly.
 		/* The quick brown fox jumped over a lazy dog.
 		   Quick wafting zephyrs vex bold Jim */
-		System.out /* testing */ .println(""Hard to believe, eh?" );
+		System.out /* testing */ .println("Hard to believe, eh?");
 		System.out.println("Surprised? /* abcde */ Or what did you expect?");
 		System.out.println("\\ // -=- \\ //");
 		System.out.println("\\\\ \\\\\\ \\\\\\\\"); // it takes 2 to make 1
